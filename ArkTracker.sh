4a09eacdca461946b972fa73f52bb3d7cf33a19f
Arif Kızıltepe <kzltpsgm@gmail.com>
2021-03-28 14:47:49 +0300

    Update ArkTracker.sh

--- ArkTracker.sh
+++ ArkTracker.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
-# Açıklama : Debian errata parser
+# Description : Debian errata parser
 #
-# Yazar: Arif KIZILTEPE
+# Author: Arif KIZILTEPE
 #         kzltpsgm@gmail.com
 #
-# Oluşturma Tarihi: 2021-03-28
-# Gereksinim;
+# Created Date : 2021-03-28
+# Requirement;
 # apt-get install jq wget curl
 # 
-# v1.0 
-#Temporary çalışma dizini oluşturuluyor
+# Betav1.0 
+
 
 #Coler variables
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
+
+
 
 echo "Please Choose Urgency Level" 
 proc(){
@@ -66,38 +68,34 @@ else
 	urques
 fi 
 
+
+echo $(date) "Work directory  is Creating..."
+sleep 1
 if test -d "tmperratafiles"; 
 then
     rm -rf tmperratafiles
-	mkdir tmperratafiles && cd tmperratafiles && touch deberrata.log
-	echo -ne '#                     (0%)\r'
-	echo $(date) "ArkTracker is Preparing..."  &>> deberrata.log
+	mkdir tmperratafiles && cd tmperratafiles
+
 else
-	mkdir tmperratafiles && cd tmperratafiles && touch deberrata.log
-	echo -ne '#                     (0%)\r'
-	echo $(date) "ArkTracker is Preparing..."  &>> deberrata.log
+	mkdir tmperratafiles && cd tmperratafiles
+
 
 fi
 
 
 
 
-echo -ne '#                     (2%)\r'
-echo $(date) "Updateable Files are Checking..."  &>> deberrata.log
+echo $(date) "Updateable Files are Checking..." && echo $(date) "Updateable Files are Checking..."  &>> ArkTracker.log
 sleep 2
 
 dpkg --get-selections | xargs apt-cache policy {} | grep -1 Installed | sed -r 's/(:|Installed: |Candidate: )//' | uniq -u | tac | sed '/--/I,+1 d' | tac | sed '$d' | sed -n 1~2p &>> debupdateable.txt
-echo bash >> debupdateable.txt
-
 for i in $(cat debupdateable.txt); do
-        dpkg --get-selections | grep -m1 $i| xargs apt-cache policy {} | grep -1 Installed &>> deberrata.log
+        dpkg --get-selections | grep -o $i| xargs apt-cache policy {} | grep -1 Installed | head -n 4 &>> ArkTracker.log 
 done
 
 
 
-echo -ne '#                     (4%)\r'
-echo $(date) "Security Tracker Json File is Downloading..."  &>> deberrata.log
-sleep 2
+echo $(date) "Security Tracker Json File is Downloading..." && echo $(date) "Security Tracker Json File is Downloading..." &>> ArkTracker.log 
 wget  https://security-tracker.debian.org/tracker/data/json -O security-tracker.json &>> deberrata.log && 
 jq 'keys' security-tracker.json > security-tracker.list
 
@@ -109,28 +107,21 @@ sed -i -e 's/://g' CVE.list
 sed -i -e 's/{//g' CVE.list
 CVENum=$(wc -l CVE.list | awk '{print $1}' )
 Dupablenum=$(wc -l debupdateable.txt | awk '{print $1}')
-
-
-echo -ne '#                     (8%)\r'
-echo $(date) "Found $Dupablenum Updateable Package and $CVENum Security Bug"  &>> deberrata.log
-sleep 2
-
 firstp=$(cat debupdateable.txt | head -n 1)
 
 
-echo -ne "#                     (10%)\r" 
-echo $(date) "Checking $firstp Package"  &>> deberrata.log
-sleep 2
+echo $(date) "Found $Dupablenum Updateable Package and $CVENum Security Bug" && echo $(date) "Found $Dupablenum Updateable Package and $CVENum Security Bug"  &>> ArkTracker.log 
+sleep 3
 
 
 
 
+
+echo $(date) "Checking $firstp Package"  && echo $(date) "Checking $firstp Package"  &>> ArkTracker.log
 
 for a in $(cat debupdateable.txt); do
-
-
-
-
+	
+	echo $(date) "Checking $a Package" && echo $(date) "Checking $a Package" &>> ArkTracker.log
 
 
 	
@@ -146,16 +137,22 @@ for a in $(cat debupdateable.txt); do
 		sed -i -e 's/\[//g' CVE.list
 		sed -i -e 's/\]//g' CVE.list
 		for r in $(cat CVE.list); do
-			#r=$(echo ${r} | sed 's/://' | sed 's/{//')
-
-			jq .$a.$r  security-tracker.json  | grep $urgency && echo $a $r
+			#r=$(echo ${r} | sed 's/://' | sed 's/{//')  
+			RevLookup="$(jq .$a.$r  security-tracker.json  | grep $urgency)"
+			if [[ "$RevLookup" ]]; 
+			then
+				echo -e "\n"
+				jq .$a.$r.releases.buster.repositories.buster security-tracker.json | xargs -i echo "Installable Version {}"
+				a=$(echo $a |sed  -e 's/"//g')
+				dpkg -s $a | grep '^Version:' |sed  -e 's/Version:/'$a' Installed Version/g'
+				r=$(echo $r |sed  -e 's/"//g')
+				echo "Bug Detail --> https://security-tracker.debian.org/tracker/$r"
+			fi
 			#jq '.$a.$r.releases.buster , select( .urgency == "unimportant" )' security-tracker.json
 
 		done
 	fi
-
 done
-echo -ne '#######################   (100%)\r'
-echo -ne '\n'
 
+echo -e "\nArk Tracker Process Finished. You can see detail $(pwd)/ArkTracer.log"
 
